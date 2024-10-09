98b581db11ab019d9e08c7f3cf7832fe88ea2422
clarkkkkk <juanfarrel0404@gmail.com>
2024-10-10 00:48:16 +0700

    feat: implement sanctum for build stateless api

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::apiResource('users', UserController::class);
