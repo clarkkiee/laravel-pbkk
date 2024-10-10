8b8bed05bbad9d5880bbc99ac5e0900dc4adb2f3
clarkkkkk <juanfarrel0404@gmail.com>
2024-10-11 03:34:31 +0700

    chore: update package and deps

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'dark-main': '#3d405b',
+                'accent': '#e07a5f',
+                'neutral': '#f4f1de'
+            }
+        },
+    },
+
+    plugins: [forms],
+};
