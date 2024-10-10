f002337295475ff7572bad12deead1f61d4505ca
clarkkkkk <juanfarrel0404@gmail.com>
2024-10-11 03:30:02 +0700

    fix: adjust migrations file

--- database/migrations/2024_10_09_154506_create_participant_table.php
+++ database/migrations/2024_10_09_154506_create_participant_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('participant', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default(DB::raw('gen_random_uuid()'));
             $table->foreignUuid('event_id')->constrained('event')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamp('joined_at')->useCurrent();
