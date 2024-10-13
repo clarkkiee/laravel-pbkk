b5a09518f433caf52124baf07744e583644d1312
clarkkkkk <juanfarrel0404@gmail.com>
2024-10-14 00:02:06 +0700

    feat: add event delete, update and dashboard event list routes

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/dashboard', [UserController::class, 'getMyEvents'])->name('dashboard');
 
     Route::resource('event', EventController::class);
     Route::get('/event', [EventController::class, 'index'])->name('events.index');
     Route::post('/event', [EventController::class, 'store'])->name('events.store');
-
+    
     Route::post('/event/{id}/join', [EventController::class, 'join'])->name('events.join');
     Route::post('/event/{id}/leave', [EventController::class, 'leave'])->name('events.leave');
-    // Route::post('/participant/join', [ParticipantController::class, 'store'])->name('participants.store');
+    Route::delete('/event/{id}/destroy', [EventController::class, 'destroy'])->name('events.destroy');
+    Route::put('/event/{id}/update', [EventController::class, 'update'])->name('events.update');
 
 });
 
