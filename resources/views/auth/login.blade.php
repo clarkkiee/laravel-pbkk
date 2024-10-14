b0f527163bcda68a319a3810e538b58006862a74
igabakarayam <ignatiusidabagusa@gmail.com>
2024-10-14 10:09:22 +0700

    tambahan login debug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,14 +47,7 @@
         <div class="flex items-center justify-end mt-4">
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
-            </button>
-        </div>
-
-        <div class="flex items-center justify-start mt-4">
-            <!-- Back Button -->
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="javascript:history.back()">
-                {{ __('Back') }}
-            </a>
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
